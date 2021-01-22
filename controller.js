9151dff09e76c94f6f91320b2d70e7e1f50d842a
yash goswami <yashgoswami2000@gmail.com>
2021-01-22 15:22:58 +0530

    gcp service account added and spreadsheet function done

--- controller.js
+++ controller.js
@@ -1,12 +1,45 @@
+const { google } = require("googleapis");
+const keys = require("assets/client_secret.json");
+
+const client = new google.auth.JWT(keys.client_email, null, keys.private_key, [
+    "https://www.googleapis.com/auth/spreadsheets",
+  ]);
+
+  client.authorize((err, tokens) => {
+    if (err) {
+      console.error(err);
+      return;
+    } else {
+      console.log("connected!");
+    }
+  });
 
 
-exports.mainfunction=(req,res)=>{
-    console.log("helllo")
-}
+  async function getSheetFromGoogle(spreadId, sheetTitle) {
+    const gsapi = google.sheets({
+      version: "v4",
+      auth: client,
+    });
+  
+    let data = await gsapi.spreadsheets.values.get({
+      spreadsheetId: spreadId,
+      range: sheetTitle,
+      majorDimension: "ROWS",
+      valueRenderOption: "FORMULA",
+      dateTimeRenderOption: "FORMATTED_STRING", //'SERIAL_NUMBER',
+      alt: JSON,
+      prettyPrint: true,
+    });
+  
+    return data;
+  }
 
 exports.FormCreate=(req,res)=>{
     var patt = new RegExp("d/(.*)/");
     var ress = patt.exec(req.body.spreadsheetUrl);
     var spreadId = ress ? ress[1] : req.body.spreadsheetUrl;
-    res.send({"request":spreadId})
+    var data=getSheetFromGoogle("1EXv02bhTmTAqWRfPKz1Qbz4kf2BqFyPgrkJ4WijI7eg","Sheet1").then((data)=>{
+        res.send(data.data.values);
+    })
+    
 }
