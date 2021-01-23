4ae54fab4dd2758f32d4880b85fb5379ef98c6cf
Yashggoswami <yashgoswami2000@gmail.com>
2021-01-23 18:41:12 +0530

    error correction

--- controller.js
+++ controller.js
@@ -35,19 +35,9 @@ const client = new google.auth.JWT(keys.client_email, null, keys.private_key, [
   }
 
 exports.FormCreate=(req,res)=>{
-<<<<<<< HEAD
-    var patt = new RegExp("d/(.*)/");
-    var ress = patt.exec(req.body.spreadsheetUrl);
-    var spreadId = ress ? ress[1] : req.body.spreadsheetUrl;
-    var data=getSheetFromGoogle(spreadId,req.body.sheetName).then((data)=>{
-      console.log(typeof(data.data.values[0]))
-        res.send(data.data.values);
-        // res.end('')
-=======
   
     getSheetFromGoogle(req.params.spreadsheetUrl,req.params.sheetName).then((data)=>{
         res.json(data.data.values[0]);
->>>>>>> 5b9f0f42212c06d29bc24a661153ab0790ac1763
     })
     
 }
