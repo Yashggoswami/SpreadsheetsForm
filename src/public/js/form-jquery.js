ea2a4af6fd8f257510335c3ac01992170f4c4d7f
Suryan Jain <suryanjain14@gmail.com>
2021-02-22 14:22:49 +0530

    Added section for write api

--- src/public/js/form-jquery.js
+++ src/public/js/form-jquery.js
@@ -15,21 +15,30 @@ $(document).ready(function () {
           var re = new RegExp("d/(.*)/");
           var resArray = re.exec(spreadsheetUrl);
           let spreadId = resArray ? resArray[1] : spreadsheetUrl;
-          var generatedAPIURL=`/API/r/Spreadsheet/CreateForm/${spreadId}/${sheetName}`;
+          var generatedAPIURL = `/API/r/Spreadsheet/CreateForm/${spreadId}/${sheetName}`;
+           var generatedWAPIURL = `/API/w/Spreadsheet/CreateForm/${spreadId}/${sheetName}`;
 
           $.ajax({ url: generatedAPIURL,
               success: function (result) {
-                   $('#generatedApi').attr('type', 'text');
-                   $("#add-sheet-form-button").hide();
-                   $("#generated_form").show();
-                   $('#code').text(result)
-                   $('#cform').after(result)
+                  $('#generatedApi').attr('type', 'text');
+                  $('#generatedwApi').attr('type', 'text');
+
+                  $("#add-sheet-form-button").hide();
+                  $("#generated_form").show();
+                  $('#code').text(result)
+                  $('#cform').after(result)
                   if (location.hostname === "localhost") {
                       $("#generatedApi").val(
                           `${location.protocol}//${location.hostname}:${location.port}${generatedAPIURL}`
                       );
+                      $("#generatedwApi").val(
+                          `${location.protocol}//${location.hostname}:${location.port}${generatedWAPIURL}`
+                      );
                   } else {
                       $("#generatedApi").val(`${location.hostname}${generatedAPIURL}`);
+                      $("#generatedwApi").val(
+                          `${location.protocol}//${location.hostname}:${location.port}${generatedWAPIURL}`
+                      );
                   }
                   if (response.redirect) {
                       window.location = response.redirect;
